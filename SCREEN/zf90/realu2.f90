bce2bc63fddf9498eebfaeb0a7f726f73607c4c0
jtv3 <jtv3>
2010-10-27 16:54:18 +0000

    changed array to allocatable to avoid running out of stack space

--- SCREEN/zf90/realu2.f90
+++ SCREEN/zf90/realu2.f90
@@ -2,15 +2,17 @@ subroutine realu2( gtot, npt, ibl, ibh, valtop, overlap, kr, ki, gvec, bvec,posn
   implicit none
   !
   integer, intent(in) :: gtot, npt, ibl, ibh, valtop, overlap
-  double precision, intent(in) :: kr( gtot, ibl : ibh + overlap), ki( gtot, ibl : ibh +overlap)
-  double precision, intent(in) :: bvec(3,3), posn(3,npt)
-  double precision, intent(out) :: ure( npt, ibl : ibh + overlap ), uim( npt, ibl : ibh + overlap )
+  real(kind=kind(1.d0)), intent(in) :: kr( gtot, ibl : ibh + overlap), ki( gtot, ibl : ibh +overlap)
+  real(kind=kind(1.d0)), intent(in) :: bvec(3,3), posn(3,npt)
+  real(kind=kind(1.d0)), intent(out) :: ure( npt, ibl : ibh + overlap ), uim( npt, ibl : ibh + overlap )
   !
   integer :: ipt, ib, gvec(gtot,3), ig, i
-  double precision :: drr, dri, dir, dii, gre(gtot,npt), gim(gtot,npt), dp, gcart(3)
+  real(kind=kind(1.d0)) :: drr, dri, dir, dii, dp, gcart(3)
+  real(kind=kind(1.d0)), allocatable :: gre( :, : ), gim( :, : )
   !
-  double precision, external :: rdp
+  real(kind=kind(1.d0)), external :: rdp
   !
+  allocate( gre( gtot, npt ), gim( gtot, npt ) )
   do ig=1,gtot
     gcart( : )  = 0
     do i=1,3
@@ -21,9 +23,9 @@ subroutine realu2( gtot, npt, ibl, ibh, valtop, overlap, kr, ki, gvec, bvec,posn
     enddo
     do ipt =1, npt
       dp = gcart(1)*posn(1,ipt) + gcart(2)*posn(2,ipt) + gcart(3)*posn(3,ipt)
-      gre(ig, ipt) = dcos(dp)
+      gre(ig, ipt) = cos(dp)
   ! I believe the sign convention here should be (+)
-      gim(ig, ipt) = dsin(dp)
+      gim(ig, ipt) = sin(dp)
     enddo
   enddo
   ure = 0; uim = 0
@@ -48,5 +50,6 @@ subroutine realu2( gtot, npt, ibl, ibh, valtop, overlap, kr, ki, gvec, bvec,posn
 !     end do
 !  end do
   ! 
+  deallocate( gre, gim )
   return
 end subroutine realu2
