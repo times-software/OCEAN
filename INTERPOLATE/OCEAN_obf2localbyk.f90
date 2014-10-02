88717334ab1e3182c3e803f61f45c42ed065ae6b
John Vinson <jtv3@uw.edu>
2014-10-02 13:30:49 -0400

    Redid main loop (fullgetcoeff) to help with cache/mem locality. Significant speedup.
    Now the loop is tiled over bands (OBFs) and planewaves. This allows the projectors, tau-phase, and Ylms to
    stay in at least the L2 cache.
    
    Still needs to be restructured. OBF2loc probably should be made a subroutine of or be done after qdiag.

--- INTERPOLATE/OCEAN_obf2localbyk.f90
+++ INTERPOLATE/OCEAN_obf2localbyk.f90
@@ -616,48 +616,118 @@
     complex(DP), intent( in ) :: tauphs( ng, ntau ), ylmfac( ng, numlm )
     complex(dp), intent( out ) :: o2l( nbd, nptot, ntau )
 
-    integer :: itau, ibd, ip, ilm, l, iproj, ig, ig_big, ig_by64
-    complex(dp) :: su
+    integer :: itau, ibd, ip, ilm, l, iproj, ig, ig_big, ig_by64, ib_big, ib_by64, ig_remain
 
 
-    ig_by64 = (ng/64) * 64
+!   L2 should be larger than tile_ng * tile_nb * sizeof(complex_dp)
+    integer, parameter :: tile_ng = 128
+    integer, parameter :: tile_nb = 64
+    integer, parameter :: tile_ngm1 = tile_ng - 1
+    integer, parameter :: tile_nbm1 = tile_nb - 1
+
+    complex(dp) :: su, su2( tile_ng ), su3( tile_ng )
+
+
+
+! !$OMP PARALLEL DEFAULT(NONE) &
+! !$OMP& PRIVATE( ig_big, itau, ip, ilm, l, iproj, su, ig, ig_by64, ib_big, ib_by64 ) &
+! !$OMP& SHARED( ylmfac, tauphs, ck, seanfq, o2l ) &
+! !$OMP& FIRSTPRIVATE( ntau, numlm, lml, nproj, nbd, ng )
+
     
-    do ig_big = 1, ig_by64, 64
+    ig_by64 = (ng/tile_ng) * tile_ng
+    ib_by64 = (nbd/tile_nb) * tile_nb
+
+    if( ig_by64 .eq. 0 ) goto 1110
+
+    do ib_big = 1, ib_by64, tile_nb
+    do ig_big = 1, ig_by64, tile_ng
+
+! !$OMP DO SCHEDULE(STATIC)
+    do itau = 1, ntau
+      ip = 0
+      do ilm = 1, numlm
+        l = lml( ilm )
+!          m = lmm( ilm )
+        su2( 1 : tile_ng ) = ylmfac( ig_big : ig_big + tile_ngm1, ilm ) * tauphs( ig_big : ig_big + tile_ngm1, itau )
+        do iproj = 1, nproj( l )
+          ip = ip + 1
+          su3( 1 : tile_ng ) = su2( 1 : tile_ng ) * seanfq( ig_big : ig_big + tile_ngm1, iproj, l )
+          do ibd = ib_big, ib_big+tile_nbm1
+!          do ibd = 1, nbd
+            su = 0.0_dp
+!            do ig = 1, ng
+!            do ig = ig_big, ig_big+31
+!              su = su &
+!                 + ylmfac( ig, ilm ) * tauphs( ig, itau ) * ck( ig, ibd ) * seanfq( ig, iproj, l )
+!            enddo
+            su = sum( su3( 1 : tile_ng ) * ck( ig_big : ig_big + tile_ngm1, ibd ) )
+            o2l( ibd, ip, itau ) = o2l( ibd, ip, itau ) + su
+          enddo
+        enddo
+      enddo
+    enddo
+! !$OMP ENDDO NOWAIT
+    enddo
+    enddo
+
+    if( ig_by64 .eq. ng ) then
+      goto 1111
+    endif
+
+1110 continue
+
+    ig_remain = ng - ig_by64
+
+! ig remainder, still subset of ib
+    do ib_big = 1, ib_by64, tile_nb
+! !$OMP DO SCHEDULE(STATIC)
     do itau = 1, ntau
         ip = 0
         do ilm = 1, numlm
           l = lml( ilm )
+          su2( 1 : ig_remain ) = ylmfac( ig_by64+1:ng, ilm ) * tauphs( ig_by64+1:ng, itau )
 !          m = lmm( ilm )
           do iproj = 1, nproj( l )
             ip = ip + 1
-            do ibd = 1, nbd
-            su = 0.0_dp
-!            do ig = 1, ng
-            do ig = ig_big, ig_big+64
-!              o2l( ibd, ip, itau ) = o2l( ibd, ip, itau ) &
-              su = su &
-          + ylmfac( ig, ilm ) * tauphs( ig, itau ) * ck( ig, ibd ) * seanfq( ig, iproj, l )
-            enddo
+            su3( 1 : ig_remain ) = su2( 1 : ig_remain ) * seanfq( ig_by64+1:ng, iproj, l )
+!            do ibd = 1, nbd
+          do ibd = ib_big, ib_big+tile_nbm1
+!            su = 0.0_dp
+!!            do ig = 1, ng
+!            do ig = ig_by64 + 1, ng
+!!              o2l( ibd, ip, itau ) = o2l( ibd, ip, itau ) &
+!              su = su &
+!          + ylmfac( ig, ilm ) * tauphs( ig, itau ) * ck( ig, ibd ) * seanfq( ig, iproj, l )
+!            enddo
+            su = sum( su3( 1 : ig_remain ) * ck( ig_by64+1:ng, ibd ) )
             o2l( ibd, ip, itau ) = o2l( ibd, ip, itau ) + su
           enddo
         enddo
       enddo
     enddo
+! !$OMP ENDDO
+
     enddo
 
-    if( ig_by64 .eq. ng ) return
+1111 continue
+    if( ib_by64 .eq. nbd ) then
+      goto 1112
+    endif
 
+! remainder of ib, all of ig
+
+! !$OMP DO SCHEDULE(STATIC)
     do itau = 1, ntau
-        ip = 0 
-        do ilm = 1, numlm
-          l = lml( ilm )
+      ip = 0 
+      do ilm = 1, numlm
+        l = lml( ilm )
 !          m = lmm( ilm )
-          do iproj = 1, nproj( l )
-            ip = ip + 1
-            do ibd = 1, nbd
+        do iproj = 1, nproj( l )
+          ip = ip + 1
+          do ibd = ib_by64 + 1, nbd
             su = 0.0_dp
-!            do ig = 1, ng
-            do ig = ig_by64 + 1, ng
+            do ig = 1, ng
 !              o2l( ibd, ip, itau ) = o2l( ibd, ip, itau ) &
               su = su &
           + ylmfac( ig, ilm ) * tauphs( ig, itau ) * ck( ig, ibd ) * seanfq( ig, iproj, l )
@@ -667,6 +737,11 @@
         enddo
       enddo
     enddo
+! !$OMP ENDDO
+
+1112 continue
+
+! !$OMP END PARALLEL
 
 !
 !
