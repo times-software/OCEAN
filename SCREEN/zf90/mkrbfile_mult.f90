7b8678ae08988e51142a53e27137a672e59c023b
John Vinson <jtv3@uw.edu>
2015-03-19 09:02:25 -0400

    Basic skeleton of allowing points for \chi(r,r') to be generated on a regular grid in real-space instead of spherical grid. Right now this breaks ocean_builder because of the calculation of ninduced in that routine.

--- SCREEN/zf90/mkrbfile_mult.f90
+++ SCREEN/zf90/mkrbfile_mult.f90
@@ -12,23 +12,33 @@ program mkrbfile
   integer :: nang, nr, indx, ntau, itau
   real(kind=kind(1.d0)) :: rmax, avec(3,3)
   real(kind=kind(1.d0)), allocatable :: posn(:,:), wpt(:), drel(:)
+  character(len=3) :: grid_type
 !
 !
 !      read(stdin,*) rmax, nr, element, indx
 !
+  grid_type = 'wom'
   open(unit=98,file='mkrb_control',form='formatted',status='old')
   read(98,*) rmax, nr
   read(98,*) ntau
+
+  write(6,*) 'Using grid type:', grid_type
 
 
   open(unit=99,file='avecsinbohr.ipt',form='formatted',status='old')
   read(99,*) avec(:,:)
   close(99)
 !
-  open(unit=99,file='specpnt',form='formatted',status='old')
-  read(99,*) nang
-  close(99)
+  select case(grid_type )
+    case( 'box' )
+      nang = nr*nr
+    case default
+      open(unit=99,file='specpnt',form='formatted',status='old')
+      read(99,*) nang
+      close(99)
 !
+  end select
+
   allocate( posn(3,nr*nang), wpt(nr*nang), drel(nr*nang) )
 !
 
@@ -39,7 +49,12 @@ program mkrbfile
   do itau = 1, ntau
     read(98,*) element, indx
     write(6,*) element, indx
-    call mkcmesh(nang, nr, rmax, element, indx, posn, wpt, drel, avec)
+    select case( grid_type )
+    case( 'box')
+      call mkbmesh( nr, rmax, element, indx, posn, wpt, drel, avec)
+    case default
+      call mkcmesh(nang, nr, rmax, element, indx, posn, wpt, drel, avec)
+    end select
 !
     write(97) posn
     write(97) wpt
