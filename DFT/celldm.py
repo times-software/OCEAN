27e3c49a3103e6c4b5690e6cebf5e070aa9f118f
John Vinson <jtv3@uw.edu>
2015-07-16 17:07:47 -0400

    Pulled more out of QEPREP. Now QEPREP only contains compiled routines that tie deep into QE or are rewrites of QE routines.

--- DFT/celldm.py
+++ DFT/celldm.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python
+# Copyright (C) 2014 OCEAN collaboration
+#
+# This file is part of the OCEAN project and distributed under the terms 
+# of the University of Illinois/NCSA Open Source License. See the file 
+# `License' in the root directory of the present distribution.
+#
+#
+
 
 import sys
 import os
