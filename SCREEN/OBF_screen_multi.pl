d5523b355aca9170e43af899291d42a8339bc005
John Vinson <jtv3@uw.edu>
2015-09-25 12:59:50 -0400

    Copying in 'dft' file to be compatible with changes to core_level.pl script

--- SCREEN/OBF_screen_multi.pl
+++ SCREEN/OBF_screen_multi.pl
@@ -41,7 +41,7 @@ my @CommonFiles = ("znucl", "paw.hfkgrid", "paw.fill", "paw.opts", "pplist", "pa
                    "ntype", "natoms", "typat", "taulist", "nedges", "edges", "caution", 
                    "epsilon", "k0.ipt", "ibase", "scfac", "rscale", "rprim", "para_prefix", 
                    "paw.nbands", "core_offset", "paw.nkpt", "pool_control", "ham_kpoints", 
-                   "cnbse.rad" );
+                   "cnbse.rad", "dft" );
 my @ExtraFiles = ("specpnt", "Pquadrature" );
 my @DFTFiles = ("rhoofr", "nscf.out", "system.rho.dat");
 
