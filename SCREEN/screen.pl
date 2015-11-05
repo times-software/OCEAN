e1ce06c661a57c5546f0fd88ee71a530e573c6c1
John Vinson <jtv3@uw.edu>
2015-11-05 15:04:09 -0500

    Screening sections should now work with spin=2. This has only been tested using a paramagnetic system to make sure that the same results are obtained with spin=1 and spin=2. Awaiting a better test case.

--- SCREEN/screen.pl
+++ SCREEN/screen.pl
@@ -20,7 +20,7 @@ if (! $ENV{"OCEAN_WORKDIR"}){ $ENV{"OCEAN_WORKDIR"} = `pwd` . "../" ; }
 ###########################
 
 
-my @CommonFiles = ("znucl", "paw.hfkgrid", "paw.fill", "paw.opts", "pplist", "paw.shells", "ntype", "natoms", "typat", "taulist", "nedges", "edges", "caution", "epsilon", "k0.ipt", "ibase", "scfac", "core_offset", "dft", "avecsinbohr.ipt", "para_prefix" );
+my @CommonFiles = ("znucl", "paw.hfkgrid", "paw.fill", "paw.opts", "pplist", "paw.shells", "ntype", "natoms", "typat", "taulist", "nedges", "edges", "caution", "epsilon", "k0.ipt", "ibase", "scfac", "core_offset", "dft", "avecsinbohr.ipt", "para_prefix", "nspin" );
 
 my @DenDipFiles = ("rhoofg", "bvecs", "efermiinrydberg.ipt");
 my @DenDipFiles2 = ( "masterwfile", "listwfile", "enkfile", "kmesh.ipt", "brange.ipt" );
