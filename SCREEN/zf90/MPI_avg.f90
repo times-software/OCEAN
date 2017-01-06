6c405b72865c65646f983b3b4a47469268de5e2e
John Vinson <jtv3@uw.edu>
2017-01-06 11:50:30 -0500

    Switching from use mpi to include mpif.h
    This is the currently preferred method for ocean2

--- SCREEN/zf90/MPI_avg.f90
+++ SCREEN/zf90/MPI_avg.f90
@@ -1,4 +1,4 @@
-! Copyright (C) 2015 OCEAN collaboration
+! Copyright (C) 2015, 2017 OCEAN collaboration
 !
 ! This file is part of the OCEAN project and distributed under the terms 
 ! of the University of Illinois/NCSA Open Source License. See the file 
@@ -7,7 +7,7 @@
 !
 program avg
 
-  use mpi
+  include 'mpif.h'
 
   integer ::ng,i,j,numsites
   integer, allocatable :: gvec(:,:),elnum(:)
