b4887606769fa190803d2658a8d6e373fd75f97b
John Vinson <jtv3@uw.edu>
2017-01-27 17:29:39 -0500

    Changing formreytab to a subroutine

--- SCREEN/zxipp/zf90/formreytab.f90
+++ SCREEN/zxipp/zf90/formreytab.f90
@@ -1,10 +1,20 @@
-program formreytab
+! Copyright (C) 2017 OCEAN collaboration
+!
+! This file is part of the OCEAN project and distributed under the terms 
+! of the University of Illinois/NCSA Open Source License. See the file 
+! `License' in the root directory of the present distribution.
+!
+!
+subroutine formreytab( nang, xpt, ypt, zpt, ytab, maxydim )
   implicit none
+  !
+  integer, intent( in ) :: nang, maxydim
+  real( kind = kind( 1.0d0 ) ), intent( in ), dimension( nang ) :: xpt, ypt, zpt
+  real( kind = kind( 1.0d0 ) ), intent( out ) :: ytab( nang, maxydim )
   !
   real( kind = kind( 1.0d0 ) ) :: prefs( 0 : 1000 )
   !
-  integer :: i, npt, nptvu, indx
-  real( kind = kind( 1.0d0 ) ), allocatable, dimension( : ) :: xpt, ypt, zpt, wpt
+  integer :: i, nptvu, indx
   real( kind = kind( 1.0d0 ) ), allocatable, dimension( : ) :: xptvu, yptvu, zptvu, wptvu
   !
   integer :: ltab( 9 ), mtab( 9 )
@@ -17,15 +27,7 @@ program formreytab
   !
   call getprefs( prefs )
   !
-  open( unit=99, file='specpnt', form='formatted', status='unknown' )
-  rewind 99
-  read ( 99, * ) npt
-  allocate( xpt( npt ), ypt( npt ), zpt( npt ), wpt( npt ) )
-  do i = 1, npt
-     read ( 99, * ) xpt( i ), ypt( i ), zpt( i ), wpt( i )
-  end do
-  close( unit=99 )
-  !
+#if 0
   open( unit=99, file='specvu', form='formatted', status='unknown' )
   rewind 99
   read ( 99, * ) nptvu
@@ -34,6 +36,7 @@ program formreytab
      read ( 99, * ) xptvu( i ), yptvu( i ), zptvu( i ), wptvu( i )
   end do
   close( unit=99 )
+#endif
   !
   ltab( 1 ) = 0; ltab( 2 : 4 ) = 1; ltab( 5 : 9 ) = 2
   mtab( 1 ) = 0; mtab( 2 ) = -1; mtab( 3 ) = 0; mtab( 4 ) = +1
@@ -57,10 +60,12 @@ program formreytab
   c( 8, 9 ) = +rm1 / sqrt( 2.0d0 )
   !
   do indx = 1, 9
-     do i = 1, npt
+     do i = 1, nang
         call getrey( rey, imy, c, indx, ltab, mtab, xpt( i ), ypt( i ), zpt( i ), prefs )
-        write ( 6, '(2(1x,1e15.8))' ) rey, imy
+!        write ( 6, '(2(1x,1e15.8))' ) rey, imy
+        ytab( i, indx ) = rey
      end do
+#if 0
      open( unit=20+indx, form='formatted', status='unknown' )
      rewind 20 + indx
      do i = 1, nptvu
@@ -69,9 +74,10 @@ program formreytab
         write ( 20+indx, '(6(1x,1f10.5))' ) xptvu( i ), yptvu( i ), zptvu( i ), rpos, rneg, imy
      end do
      close( unit=20+indx )
+#endif
   end do
   !
-end program formreytab
+end subroutine formreytab
   !
 subroutine getrey( rey, imy, c, indx, ltab, mtab, x, y, z, prefs )
   implicit none
@@ -91,7 +97,7 @@ subroutine getrey( rey, imy, c, indx, ltab, mtab, x, y, z, prefs )
   do i = 1, 9
      call ylmeval( ltab( i ), mtab( i ), x, y, z, ylm, prefs )
      rey = rey + c( i, indx ) * ylm
-     write ( 40 + indx, '(8(1x,1f10.5))' ) x, y, z, ylm * c( i, indx ), ylm, c( i, indx )
+!     write ( 40 + indx, '(8(1x,1f10.5))' ) x, y, z, ylm * c( i, indx ), ylm, c( i, indx )
      imy = imy - rm1 * c( i, indx ) * ylm
   end do
   !
