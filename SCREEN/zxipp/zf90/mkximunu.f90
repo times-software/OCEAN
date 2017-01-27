d5bea7da40dd5ee6b42e48f09c642557dcb7d726
John Vinson <jtv3@uw.edu>
2017-01-27 16:32:03 -0500

    Shirley's program to project xi_0 onto lm, r basis 	new file:   mkximunu.f90

---
+++ SCREEN/zxipp/zf90/mkximunu.f90
@@ -0,0 +1,87 @@
+! Copyright (C) 2017 OCEAN collaboration
+!
+! This file is part of the OCEAN project and distributed under the terms 
+! of the University of Illinois/NCSA Open Source License. See the file 
+! `License' in the root directory of the present distribution.
+!
+!
+program mkximunu
+  implicit none
+  !
+  integer :: nr, nang, nang2, i, j, ir, jr, ia, ja, chan1, chan2
+  real( kind = kind( 1.d0 ) ) :: ener
+  complex( kind = kind( 1.0d0 ) ) :: csu, rm1
+  character * 8 :: fnam
+  !
+  real( kind = kind( 1.0d0 ) ), allocatable :: rad( : ), x( :, : ), ymu( :, : )
+  real( kind = kind( 1.0d0 ) ), allocatable :: rerow( :, : ), imrow( :, : )
+  complex( kind = kind( 1.0d0 ) ), allocatable :: xi( :, :, :, : )
+  !
+  rm1 = -1
+  rm1 = sqrt( rm1 )
+  !
+  read ( 5, * ) ener, chan1, chan2
+  !
+  open( unit=99, file='projsupp', form='formatted', status='unknown' )
+  rewind 99
+  read ( 99, * ) nr, nang
+  allocate( rad( nr ), x( 4, nang ), xi( nang, nr, nang, nr ), rerow( nang, nr ), imrow( nang, nr ) )
+  read ( 99, * ) rad( : )
+  close( unit=99 )
+  !
+  open( unit=99, file='specpnt', form='formatted', status='unknown' )
+  rewind 99
+  read ( 99, * ) nang2
+  if ( nang .ne. nang2 ) stop 'nang nang2 mismatch in mkximunu'
+  read ( 99, * ) x( :, : )
+  close( unit=99 )
+  !
+  allocate( ymu( nang, 9 ) )
+  open( unit=99, file='ytab', form='formatted', status='unknown' )
+  rewind 99
+  do i = 1, 9
+     do j = 1, nang
+        read ( 99, * ) ymu( j, i )
+     end do
+  end do
+  close( unit=99 )
+  !
+  write ( 6, * ) 'reading ximat'
+  open( unit=99, file='ximat1', form='unformatted', status='unknown' )
+  rewind 99
+!  open( unit=98, file='ximat2', form='unformatted', status='unknown' )
+!  rewind 98
+  imrow( :, : ) = 0.0d0
+  do i = 1, nr
+     do j = 1, nang
+        read ( 99 ) rerow( :, : )
+!        read ( 98 ) imrow( :, : )
+        xi( :, :, j, i ) = rerow( :, : ) + rm1 * imrow( :, : )
+     end do
+  end do 
+  close( unit=99 )
+  write ( 6, * ) 'done reading ximat'
+  !
+! do i = 1, 9
+!    do j = 1, 9
+  i = chan1; j = chan2
+        write ( fnam, '(1a6,2i1)' ) '.ymunu', i, j
+        open( unit=99, file=fnam, form='formatted', status='unknown' )
+        rewind 99
+        do ir = 1, nr
+           do jr = 1, nr
+              csu = 0.0d0
+              do ia = 1, nang
+                 do ja = 1, nang
+                    csu = csu + xi( ia, ir, ja, jr ) * ymu( ia, i ) * x( 4, ia ) * ymu( ja, j ) * x( 4, ja )
+                 end do
+              end do
+              write ( 99, '(5(1x,1e15.8))' ) rad( ir ), rad( jr ), csu, ener
+           end do
+           write ( 99, * )
+        end do
+        close( unit=99 )
+!    end do
+! end do
+  !
+end program mkximunu
