0516b74a5c1974801634d1b7207e72072763badc
John Vinson <jtv3@uw.edu>
2017-11-15 17:12:03 -0500

    Cleaning opf.pl
    Reducing the amound of cruft, simplifying.

--- OPF/opf.pl
+++ OPF/opf.pl
@@ -28,10 +28,6 @@ my @CommonFiles = ("znucl", "opf.hfkgrid", "opf.fill", "opf.opts", "pplist", "sc
                    "k0.ipt", "ibase", "scfac", "calc" );
 
 
-my @DenDipFiles = ("rhoofg", "bvecs", "efermiinrydberg.ipt");
-my @DenDipFiles2 = ( "masterwfile", "listwfile", "enkfile", "kmesh.ipt", "brange.ipt" );
-
-my @ExtraFiles = ("specpnt", "Pquadrature" );
 
 my $runOBF = 1;
 if (-e "done" ) {
@@ -56,16 +52,6 @@ foreach (@CommonFiles) {
   `cp ../Common/$_ .` == 0 or die "Failed to get $_ from Common/\n";
 }
 
-foreach (@ExtraFiles) {
-  `cp $ENV{'OCEAN_BIN'}/$_ .` == 0 or die;
-}
-
-open PPLIST, "pplist" or die "Failed to open pplist\n";
-while (<PPLIST>) {
-  chomp;
-  s/\s+//;
-  `cp "../$_" .`;
-}
 
 if( open CALC, "calc" )
 {
@@ -110,42 +96,35 @@ unless( -d "zpawinfo" )
 ###################################
 
 
-# PP mod
-###################################
-print "Converting psp format\n";
-open SITELIST, "sitelist" or die "Failed to open sitelist\n";
-<SITELIST> =~ m/\s*(\d+)/ or die "Malformaed sitelst\n";
-my $numsites = $1;
 my $ppfilename;
-my $elname;
-my $elnum;
 my $ppmodname;
-for (my $i=0; $i < $numsites; $i++ ) {
-  <SITELIST> =~ m/\s*(\S+)\s+(\d+)\s+(\S+)/ or die "Malformaed sitelst\n";
-  $elname = $1;
-  $elnum = $2;
-  $ppfilename = $3;
-  $ppmodname = $ppfilename . ".mod";
-  system("echo '$ppfilename\n$ppmodname' | $ENV{'OCEAN_BIN'}/fhi2eric.x") == 0 
-      or die "Failed to convert psp file $ppfilename\n";
-}
-close SITELIST;
-###################################
 
 # Load up pspoptions
 ###################################
 open PSPO, "pspoptions" or die "Failed to open pspoptions\n";
 my %pspopts;
 my %pspfill;
+my %psplist;
 while (<PSPO>) {
-  $_ =~ m/\s*(\d+)\s+(\w+)\s+\S+\s+(\S+)\s+(\S+)/ or die "Malformed pspoptions\n";
-  $pspopts{"$1"} = $3;
-  $pspfill{"$1"} = $4;
+  $_ =~ m/\s*(\d+)\s+(\w+)\s+(\S+)\s+(\S+)\s+(\S+)/ or die "Malformed pspoptions\n";
+  $psplist{"$1"} = $3;
+  $pspopts{"$1"} = $4;
+  $pspfill{"$1"} = $5;
   `cp "../$3" .`;
-  `cp "../$4" . `;
+  `cp "../$4" .`;
+  copy( "../$5", "$5" );
 }
 close PSPO;
 
+# PP mod
+###################################
+foreach  my $znucl (keys %psplist )
+{
+  $ppfilename = $psplist{$znucl};
+  $ppmodname = $ppfilename . ".mod";
+  system("echo '$ppfilename\n$ppmodname' | $ENV{'OCEAN_BIN'}/fhi2eric.x") == 0
+      or die "Failed to convert psp file $ppfilename\n";
+}
 
 # shells
 ##################################
@@ -175,21 +154,9 @@ close GRID;
 
 my $optionfilename;
 
-open HFINLIST, "hfinlist" or die "Failed top open hfinlist\n";
-my $hfinline;
-my $znucl;
-my $nnum;
-my $lnum;
-
-while ( $hfinline = <HFINLIST> ) {
-  ($hfinline =~ m/(\S+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\w+)\s+(\d+)/) or die "Malformed hfinlist\n";
-  $ppfilename = $1;
-  $znucl = $2;
-  $nnum = $3;
-  $lnum = $4;
-  $elname = $5;
-  $elnum = $6;  
-
+foreach my $znucl (keys %psplist )
+{
+  $ppfilename = $psplist{$znucl};
   $optionfilename = $pspopts{"$znucl"};
 #  print "$optionfilename\n";
   die "No option file for $ppfilename\n" unless (-e $optionfilename );
@@ -203,9 +170,7 @@ while ( $hfinline = <HFINLIST> ) {
   print HFIN "initgrid\n";
   print HFIN "$znucl $grid\n";
   print HFIN "ppload\nmkcorcon\nscreencore\ncalcso\nspartanfip\n";
-#  print HFIN "$nnum  $lnum\n";
   print HFIN `cat "$pspfill{"$znucl"}"`;
-#  print HFIN "calcso\n";
   print HFIN "quit\n";
   close HFIN;
 
@@ -233,16 +198,16 @@ while ( $hfinline = <HFINLIST> ) {
   print "Running hfk.x\n";
 
 
-  system("$ENV{'OCEAN_BIN'}/hfk.x < hfin1 > hfk.${znucl}_${nnum}_${lnum}.1.log") == 0 or die;
+  system("$ENV{'OCEAN_BIN'}/hfk.x < hfin1 > hfk.${znucl}.1.log") == 0 or die;
   # Check the end of the log to see if we are ok
-  my $hfk_status = `tail -n 1 hfk.${znucl}_${nnum}_${lnum}.1.log`;
+  my $hfk_status = `tail -n 1 hfk.${znucl}.1.log`;
   unless( $hfk_status =~ m/terminus/ )
   {
     die "The program hfk.x has exited incorrectly for hfin1.\nExiting ...\n";
   }
 
-  system("$ENV{'OCEAN_BIN'}/hfk.x < hfin2 > hfk.${znucl}_${nnum}_${lnum}.2.log") == 0 or die;
-  $hfk_status = `tail -n 1 hfk.${znucl}_${nnum}_${lnum}.2.log`;
+  system("$ENV{'OCEAN_BIN'}/hfk.x < hfin2 > hfk.${znucl}.2.log") == 0 or die;
+  $hfk_status = `tail -n 1 hfk.${znucl}.2.log`;
   unless( $hfk_status =~ m/terminus/ )
   {
     die "The program hfk.x has exited incorrectly for hfin2.\nExiting ...\n";
@@ -250,8 +215,8 @@ while ( $hfinline = <HFINLIST> ) {
   my $corezfile = sprintf("corezetaz%03i",$znucl);
   move("xifile","$corezfile");
 
-  system("$ENV{'OCEAN_BIN'}/hfk.x < hfin3 > hfk.${znucl}_${nnum}_${lnum}.3.log") == 0 or die;
-  $hfk_status = `tail -n 1 hfk.${znucl}_${nnum}_${lnum}.3.log`;
+  system("$ENV{'OCEAN_BIN'}/hfk.x < hfin3 > hfk.${znucl}.3.log") == 0 or die;
+  $hfk_status = `tail -n 1 hfk.${znucl}.3.log`;
   unless( $hfk_status =~ m/terminus/ )
   {
     die "The program hfk.x has exited incorrectly for hfin3.\nExiting ...\n";
@@ -259,11 +224,9 @@ while ( $hfinline = <HFINLIST> ) {
   print "Done running hfk.x\n";
 
 
-# Clean-up time
-#  `mv xifile $corezfile`;
-  unless( -d "zdiag_${znucl}_${nnum}_${lnum}" )
+  unless( -d "zdiag_${znucl}" )
   {
-    ( mkdir "zdiag_${znucl}_${nnum}_${lnum}" ) or die "$!";
+    ( mkdir "zdiag_${znucl}" ) or die "$!";
   }
 
 # Wander through the directory making some changes
@@ -272,7 +235,6 @@ while ( $hfinline = <HFINLIST> ) {
   {
     if( ( $file =~ m/^[fg]k/ ) or ( $file =~ m/^(ae|ft|ps).z/ ) or
         ( $file =~ m/^(deflin|melfile|corez)/ ) or ( $file =~ m/^(rad|prj)filez/ ) or
-#        ( $file =~ m/^(vcallel|vvallel|vc_bare|vcxxxxx|vvpseud)/ ) or 
         ( $file =~ m/^(vcallel|vvallel|vc_bare|vpseud|valence)/ ) or 
         ( $file =~ m/^coreorb/ ) )
     {
@@ -282,12 +244,11 @@ while ( $hfinline = <HFINLIST> ) {
     {
       my $dest = sprintf( "${file}z%03i", $znucl );
       move( $file, "zpawinfo/$dest" );
-#      move( $file, "zpawinfo/${file}z${znucl}" )
     }
     elsif( ( $file =~ m/^melfilez\w$/ ) or ( $file =~ m/^(sm|am|di|pr|psft|aeft)\w$/ ) or
            ( $file =~ m/^(mt|dif)\w\w$/ ) or ( $file =~ m/^(map|ex)/ ) )
     {
-      move( $file, "zdiag_${znucl}_${nnum}_${lnum}" );
+      move( $file, "zdiag_${znucl}" );
     }
   }
 
@@ -299,14 +260,6 @@ while ( $hfinline = <HFINLIST> ) {
   }
 
 
-
-#  `mv {f,g}k* shellR* {ae,ft,ps}?z* {deflin,melfile}z*n*l* {rad,prj}filez* corez* zpawinfo/`;
-#  # vcsz*
-#  `mkdir -p zdiag_${znucl}_${nnum}_${lnum}`;
-#  `mv melfilez??? mt?? {sm,am,di,pr}? dif?? map* ex* {ps,ae}ft? zdiag_${znucl}_${nnum}_${lnum}/`;
-#  `mv vcallel* vvallel* vc_bare* vcxxxxx* vvpseud* zpawinfo/`;
-#  `rm -f radpot hapot hfpot config corcon valcon chgsum atmf99 skip psld rslt tmp aprog vxcofr`;
-  last;
 }
 close HFINLIST;
 
