b86459d9722a456c699186c0689b08781909c1c6
John Vinson <jtv3@uw.edu>
2018-08-29 14:32:19 -0400

    More WIP for TPA

--- CNBSE/zcain/zf90/makeProjectorRadialIntegral.f90
+++ CNBSE/zcain/zf90/makeProjectorRadialIntegral.f90
@@ -6,11 +6,12 @@ subroutine makeProjectorRadialIntegral( npmax, lmin, lmax, nproj, atno, powmax, 
   integer, intent( inout ) :: ierr
   real(DP), intent( out ) :: radialPrj( npmax, lmin:lmax, npmax, lmin:lmax, 0 : powmax )
   !
-  integer :: dumi, nrad, ll, ll1, ll2, nu1, nu2, ip
-  real(DP) :: dumf, area, dl
+  integer :: dumi, nrad, ll, ll1, ll2, nu1, nu2, ip, ir
+  real(DP) :: dumf, area, dl, mel
   character( len=11 ) :: filnam
+  character( len=17 ) :: cornam
 
-  real(DP), allocatable :: projectors( :, :, : ), radialGrid(:), projLine(:), f(:)
+  real(DP), allocatable :: projectors( :, :, : ), radialGrid(:), projLine(:), f(:), core(:)
   !
   write( filnam, '(A,I3.3)' ) 'radfilez', atno
   open( unit=99, file=filnam, form='formatted', status='old' )
@@ -22,7 +23,7 @@ subroutine makeProjectorRadialIntegral( npmax, lmin, lmax, nproj, atno, powmax, 
   do ll = lmin, lmax
     write( filnam, '(A,I1.1,A,I3.3)' ) 'ae', ll, 'z', atno
   
-    allocate( projLine( nproj( ll )
+    allocate( projLine( nproj( ll ) ) )
     open(unit=99,file=filnam, form='formatted', status='old' )
     do ir = 1, nrad
       read(99,*) radialGrid(ir), projLine(:)
@@ -38,20 +39,21 @@ subroutine makeProjectorRadialIntegral( npmax, lmin, lmax, nproj, atno, powmax, 
 
   radialPrj( :, :, :, :, : ) = 0.0_dp
   allocate( f( nrad ) )
-  dl = 
+  dl = 0.01d0 * dlog(radialGrid(101)/radialGrid(1))
   do ip = 0, powmax
 
     do ll1 = lmin, lmax
-      do nu1 = 1, proj( ll1 )
+      do nu1 = 1, nproj( ll1 )
 
         do ll2 = lmin, lmax
-          do nu2 = 1, proj( ll2 )
-            f(:) = projectors( :, nu2, ll2 ) * projectors( :, nu1, ll1 )
+          do nu2 = 1, nproj( ll2 )
+            f(:) = projectors( :, nu2, ll2 ) * projectors( :, nu1, ll1 ) * radialGrid(:)**ip
             call bintegrate( nrad, radialGrid, dl, f, area )
-            if( ll2 .eq. ll1 ) then
-              write(6,'(4I8,X,20.12E)') ip, ll1, nu1, nu2, area
-            endif
-            radialPrj( nu2, ll2, nu1, ll1, ip ) = area
+            call rpower( nrad, radialGrid, dl, projectors( :, nu2, ll2 ), projectors( :, nu1, ll1 ), ip, mel )
+!            if( ll2 .eq. ll1 ) then
+              write(6,'(5I8,2(X,E20.12))') ip, ll1, nu1, ll2, nu2, area, mel
+!            endif
+            radialPrj( nu2, ll2, nu1, ll1, ip ) = mel
           enddo
         enddo
       
@@ -59,13 +61,32 @@ subroutine makeProjectorRadialIntegral( npmax, lmin, lmax, nproj, atno, powmax, 
     enddo
   enddo
     
+  allocate( core( nrad ) )
+  write(cornam, '(A,I3.3,A)' ) 'coreorbz', atno, 'n01l00'
+  open( unit=99, file=cornam, form='formatted', status='old' )
+    read( 99, * ) 
+  do ir = 1, nrad
+    read( 99, * ) dumf, core( ir )
+  enddo
+  close(99)
+
+  do ll1 = lmin, lmax
+    do ip = 0, powmax
+      do nu1 = 1, nproj( ll1 )
+        f(:) = core(:) * projectors( :, nu1, ll1 ) * radialGrid(:)**(ip-1)
+        call bintegrate( nrad, radialGrid, dl, f, area )
+        call rpower( nrad, radialGrid, dl, core, projectors( :, nu1, ll1 ), ip-1, mel )
+        write(6,'(3I8,2(X,E20.12))') ll1, ip, nu1, area, mel
+      enddo
+    enddo
+  enddo
 
 end subroutine
 
-subroutine bintegrate( nr, r, dl, f, area, irc )
+subroutine bintegrate( nr, r, dl, f, area )
   implicit none
   !
-  integer :: nr, irc, j, k, m
+  integer :: nr,  j, k, m
   real( kind = kind( 1.0d0 ) ) :: dl, area, su, f( nr ), r( nr )
   !  
   real( kind = kind( 1.0d0 ) ), allocatable :: rrr( : )
@@ -76,27 +97,51 @@ subroutine bintegrate( nr, r, dl, f, area, irc )
   ! 
   !
   su = 0.0d0
-  k = irc
+  k = nr
   do while ( k .gt. 0 )
-     if ( ( k .eq. irc ) .or. ( k .lt. 5 ) ) then
-        su = su + 14.0d0 * f( k ) * r( k ) !r( k ) ** 3
+     if ( ( k .eq. nr ) .or. ( k .lt. 5 ) ) then
+        su = su + 14.0d0 * f( k ) * r( k )**3 !r( k ) ** 3
      else
-        su = su + 28.0d0 * f( k ) * r( k ) !r( k ) ** 3
+        su = su + 28.0d0 * f( k ) * r( k )**3 !r( k ) ** 3
      end if
      k = k - 4
   end do
   k = k + 4
-  j = irc - 1
+  j = nr - 1
   do while ( j .gt. k )
-     su = su + 64.0d0 * f( j ) * r( j ) !r( j ) ** 3
+     su = su + 64.0d0 * f( j ) * r( j )**3 !r( j ) ** 3
      j = j - 2
   end do
-  m = irc - 2
+  m = nr - 2
   do while ( m .gt. k )
-     su = su + 24.0d0 * f( m ) * r( m ) !r( m ) ** 3
+     su = su + 24.0d0 * f( m ) * r( m )**3 !r( m ) ** 3
      m = m - 4
   end do
   area = su * dl / 45.0d0
   !
   return
 end subroutine
+
+subroutine rpower( nr, r, dl, ph1, ph2, npowr, mel )
+  implicit none
+  !
+  integer :: nr, npowr
+  real( kind = kind( 1.0d0 ) ) :: dl, mel, r( nr ), ph1( nr ), ph2( nr )
+  !
+  integer :: i, j, ii
+  real( kind = kind( 1.0d0 ) ) :: f( 0 : 4 )
+  !
+  mel = 0.d0
+
+  do i = 1, nr - 4, 4
+    do j = 0, 4
+      ii = i + j
+      f( j ) = ph1( ii ) * ph2( ii ) * r( ii ) ** ( npowr + 2 )
+    end do
+    mel = mel + 14.0d0 / 45.0d0 * dl * r( i ) * ( f( 0 ) + f( 4 ) )
+    mel = mel + 64.0d0 / 45.0d0 * dl * r( i ) * ( f( 1 ) + f( 3 ) )
+    mel = mel + 24.0d0 / 45.0d0 * dl * r( i ) * f( 2 )
+  end do
+  !
+  return
+end subroutine rpower
---
+++ CNBSE/zcain/zf90/tpa_dotter.f90
@@ -0,0 +1,163 @@
+! Copyright (C) 2014, 2018 OCEAN collaboration
+!
+! This file is part of the OCEAN project and distributed under the terms 
+! of the University of Illinois/NCSA Open Source License. See the file 
+! `License' in the root directory of the present distribution.
+!
+!
+! Modification of the dotter program to make modified mels
+! 
+! The output is a new mels as opposed to the normal output of dotter (which is semi-deprecated for ocean.x)
+program tpa_dotter
+  implicit none
+  !
+  integer, parameter :: DP = kind(1.0d0)
+  integer :: nptot, ntot, mc, i, lc, idum, nspn, ivms, iufmt, ifmt
+  real( DP ) :: rr, ri, ir, ii, br, bi, tau( 3 )
+  real( DP ), allocatable, dimension( : ) :: mer, mei
+  real( DP ), allocatable, dimension( :, : ) :: reSecondPhoton, imSecondPhoton
+  real( DP ), allocatable, dimension( :, :, : ) :: pcr, pci, reSecondary, imSecondary, reOuter, imOuter
+  character(len=9) :: cksnam
+  character(len=7) :: secondPhotonName
+  character(len=18) :: melInName, melOutName
+  logical :: have_spin
+  !
+  read ( 5, * ) cksnam
+  read ( 5, * ) melInName
+  read ( 5, * ) secondPhotonName
+  read ( 5, * ) melOutName
+  !
+  inquire(file='nspin',exist=have_spin)
+  open( unit=99, file=cksnam, form='unformatted', status='old' )
+  rewind 99
+  if( have_spin ) then
+    read ( 99 ) nptot, ntot, nspn
+  else
+    read ( 99 ) nptot, ntot
+    nspn = 1
+  endif
+  read ( 99 ) tau( : )
+  allocate( pcr( nptot, ntot, nspn ), pci( nptot, ntot, nspn ) )
+  read ( 99 ) pcr
+  read ( 99 ) pci
+  close( unit=99 )
+
+  ! First step is the < v'l'm' | band states > < bands states | v l m >
+  ! The use of the cks output (pcr,pci) is that it adds a Fermi factor
+  ! Otherwise, the | v l m > states are approximately complete for the 
+  ! localized basis and finite energy range that we've specified
+  
+  allocate( reOuter( nptot, nptot, nspn ), imOuter( nptot, nptot, nspn ) )
+  reOuter(:,:,:) = 0.0_dp
+  imOuter(:,:,:) = 0.0_dp
+
+  ! Testing option here
+  if( .false. ) then
+    do ispn = 1, nspn
+      do i = 1, nptot
+        reOuter( i, i, ispn ) = 1.0_dp
+      enddo
+    enddo
+  else
+    do ispn = 1, nspn
+      do k = 1, ntot
+        do j = 1, nptot
+          do i = 1, nptot
+            reOuter( i, j, ispn ) = reOuter( i, j, ispn ) + pcr( j, k, ispn ) * pcr( i, k, ispn ) &
+                                  + pci( j, k, ispn ) * pci( i, k, ispn )
+            imOuter( i, j, ispn ) = imOuter( i, j, ispn ) + pcr( j, k, ispn ) * pci( i, k, ispn ) &
+                                  - pci( j, k, ispn ) * pcr( i, k, ispn )
+          enddo
+        enddo
+      enddo
+    enddo
+  endif
+
+  deallocate( pcr, pci )
+
+    
+
+
+  
+  !
+  open( unit=99, file='ZNL', form='formatted', status='unknown' )
+  rewind 99
+  read ( 99, * ) idum, idum, lc
+  close( unit=99 )
+  !
+  allocate( reSecondary( nptot, nspn, -lc:lc ), imSecondary( nptot, nspn, -lc:lc ) )
+  !
+  allocate( mer( nptot ), mei( nptot ) )
+  open( unit=99, file=melInName, form='formatted', status='old' )
+  rewind 99
+  if(nspn .eq. 1) then
+     do mc = -lc, lc
+        do i = 1, nptot
+           read ( 99, * ) mer( i ), mei( i )
+        end do
+        do i = 1, nptot
+           rr = dot_product( mer( : ), reOuter( :, i, 1 ) )
+           ri = dot_product( mer( : ), imOuter( :, i, 1 ) )
+           ir = dot_product( mei( : ), reOuter( :, i, 1 ) )
+           ii = dot_product( mei( : ), imOuter( :, i, 1 ) )
+           br = rr - ii
+           bi = -ri - ir
+           reSecondary( i, 1, mc ) = br
+           imSecondary( i, 1, mc ) = bi
+        end do
+     end do
+  else
+    write(6,*) 'Spin not supported yet!!!!!!!!'
+    stop
+     do mc = -lc, lc
+        do i = 1, nptot
+           read ( 99, * ) mer( i ), mei( i )
+        end do
+        do ivms = 1 , 2
+           do i = 1, ntot
+!              rr = dot_product( mer( : ), pcr( :, i, ivms ) )
+!              ri = dot_product( mer( : ), pci( :, i, ivms ) )
+!              ir = dot_product( mei( : ), pcr( :, i, ivms ) )
+!              ii = dot_product( mei( : ), pci( :, i, ivms ) )
+              br = rr - ii
+              bi = -ri - ir
+           end do
+        enddo
+     end do     
+  endif
+  close(99)
+
+  deallocate( reOuter, imOuter, mer, mei )
+  allocate( reSecondPhoton( nptot, nptot ), imSecondPhoton( nptot, nptot ) )
+  open(unit=99, name=secondPhotonName, form='formatted', status='old' )
+  do i = 1, nptot
+    do j = 1, nptot
+      read( 99, * ) reSecondPhoton( j, i ), imSecondPhoton( j, i )
+    enddo
+  enddo
+  close( 99 )
+
+  open( unit=99, file=melOutName, form='formatted', status='unknown' )
+  if( npsn .eq. 1 ) then
+    do mc = -lc, lc
+      do i = 1, nptot
+        rr = dot_product( reSecondary( :, 1, mc ), reSecondPhoton( :, i ) )
+        ri = dot_product( reSecondary( :, 1, mc ), imSecondPhoton( :, i ) )
+        ir = dot_product( imSecondary( :, 1, mc ), reSecondPhoton( :, i ) )
+        ii = dot_product( imSecondary( :, 1, mc ), imSecondPhoton( :, i ) )
+          
+        ! secondPhoton is starred
+        br = rr + ii
+        bi = ri - ir
+  
+        write( 99, * ) br, bi
+      enddo
+    enddo
+  else
+    stop
+  endif
+  close( 99 )
+  deallocate( reSecondPhoton, imSecondPhoton, reSecondary, imSecondary )
+  !
+  !
+end program dotter
