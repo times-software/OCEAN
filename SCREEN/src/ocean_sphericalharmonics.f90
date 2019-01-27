7678eb57db3b7913612975f5fd985edf003c9e2f
jtv3 <jtv3@uw.edu>
2019-01-26 19:16:17 -0500

    Add L=3 and fixed m ordering

--- SCREEN/src/ocean_sphericalharmonics.f90
+++ SCREEN/src/ocean_sphericalharmonics.f90
@@ -34,26 +34,53 @@ module ocean_sphericalHarmonics
 
         select case ( m )
           case( 1 )
-            ylm = pref * sqrt(3.0_DP) * y
-          case( -1 )
-            ylm = pref * sqrt(3.0_DP) * z
-          case( 0 )
             ylm = pref * sqrt(3.0_DP) * x
+!            ylm = pref * sqrt(3.0_DP) * y
+          case( -1 )
+            ylm = pref * sqrt(3.0_DP) * y
+!            ylm = pref * sqrt(3.0_DP) * z
+          case( 0 )
+            ylm = pref * sqrt(3.0_DP) * z
+!            ylm = pref * sqrt(3.0_DP) * x
         end select 
 
       case( 2 )
 
         select case ( m )
-          case( 0 )
-            ylm = pref * sqrt(15.0_DP) * x * y
-          case( 2 )
-            ylm = pref * sqrt(15.0_DP) * y * z
+!          case( 0 )
           case( -2 )
-            ylm = pref * sqrt(5.0_DP)/2.0_DP * ( 2.0_DP * z**2 - x**2 -y**2 )
+            ylm = pref * sqrt(15.0_DP) * x * y
+!          case( 2 )
+          case( -1 )
+            ylm = pref * sqrt(15.0_DP) * y * z
+!          case( -2 )
+          case( 0 )
+!            ylm = pref * sqrt(5.0_DP)/2.0_DP * ( 2.0_DP * z**2 - x**2 -y**2 )
+            ylm = pref * sqrt(5.0_DP)/2.0_DP * ( 3.0_DP * z**2 - 1.0_DP )
           case( 1 )
             ylm = pref * sqrt(15.0_DP) * z * x
-          case( -1 )
+!          case( -1 )
+          case( 2 )
             ylm = pref * sqrt(15.0_DP)/2.0_DP * ( x**2 - y**2 )
+        end select
+
+      case( 3 )
+        
+        select case( m )
+          case( -3 )
+            ylm = pref * 0.5_dp * sqrt( 35.0_DP * 0.5_DP ) * ( 3.0_DP * x**2 - y**2 ) * y
+          case( -2 )
+            ylm = pref * sqrt( 105.0_DP ) * x * y * z
+          case( -1 ) 
+            ylm = pref * 0.5_dp * sqrt( 21.0_DP * 0.5_DP ) * y * ( 4.0_DP * z**2 - x**2 - y**2 )
+          case( 0 ) 
+            ylm = pref * 0.5_DP * sqrt( 7.0_DP ) * z * ( 2.0_DP*z**2 - 3.0_DP * x**2 - 3.0_DP * y**2 )
+          case( 1 )
+            ylm = pref * 0.5_DP * sqrt( 21.0_DP * 0.5_DP ) * x * ( 4.0_DP * z**2 - x**2 - y**2 )
+          case( 2 ) 
+            ylm = pref * 0.5_DP * sqrt( 105.0_DP ) * ( x**2 - y**2 ) * z
+          case( 3 )
+            ylm = pref * 0.5_DP * sqrt( 35.0_DP * 0.5_DP ) * ( x**2 - 3.0_DP * y**2 ) * x
         end select
 
     end select
